5aa8a4fbec82817bdef60df06292cf6c7d2d70d0
brssnj45 <brssnj45@gmail.com>
2021-09-20 23:27:40 -0400

    added responsive json

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,18 @@ const Pet = require('../models/pet');
 
 module.exports = (app) => {
 
-/* GET home page. */
-app.get('/', (req, res) => {
-  const page = req.query.page || 1
+  app.get('/', (req, res) => {
+    const page = req.query.page || 1
 
-  Pet.paginate({}, {page: page}).then((results) => {
-    res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page, term: req.query.term });    
+    Pet.paginate({}, { page: page }).then((results) => {
+      // If the request is JSON, we want to send a JSON response
+      if (req.header('Content-Type') == 'application/json') {
+        return res.json({ pets: results.docs, pagesCount: results.pages, currentPage: page });
+      // Otherwise we do what we did before
+      } else {
+        res.render('pets-index', { pets: results.docs, pagesCount: results.pages, currentPage: page });
+      }
+    });
   });
-});
-
 }
+
